58e57f0eb9746cc9801a543d89fe360e94048d32
Lucas Jara <lucasjaraespinoza@gmail.com>
2019-02-10 19:04:30 -0300

    Ajustes para webservice , limpieza de formularios

--- RegistroDeudas/RegistroDeudas/Apps/Home.cs
+++ RegistroDeudas/RegistroDeudas/Apps/Home.cs
@@ -11,7 +11,12 @@ namespace RegistroDeudas.Apps
     {
         Dictionary<string, int> NombreCategoria = new Dictionary<string, int>
         {
-            { "Compras", 1 }, { "Ventas", 2 }
+            { "Ahorros", 1 }, { "Arriendo", 2 },
+            { "Aseo Personal", 3 }, { "Juegos", 4 },
+            { "Tecnologia", 5 }, { "Regalos", 6 },
+            { "Telefono", 7 }, { "Prestamo", 8 },
+            { "Transporte", 9 }, { "Supermercado", 10 },
+            { "Muebles", 11 }, { "Otros", 12 }
         };
 
         public Home()
@@ -31,7 +36,7 @@ namespace RegistroDeudas.Apps
                 Source = "money.png"
             };
             // Botones Tipo Acción
-            Button btn_compra = new Button {Text = "Compras" };
+            Button btn_compra = new Button { Text = "Compras" };
             Button btn_venta = new Button { Text = "Ventas" };
             Button btn_transferencia = new Button { Text = "Transferencias" };
 
@@ -45,29 +50,20 @@ namespace RegistroDeudas.Apps
             grid.Children.Add(stack_botones, 1, 0);
 
             var margen = new Thickness(50, 0, 50, 0);
-            Label lbl_descripcion = new Label { Text = "Descripción:", Margin = margen };
+            Label lbl_descripcion = new Label { Text = "Descripción:", Margin = margen, FontSize = 18, FontAttributes = FontAttributes.Bold };
             Entry ent_descripcion = new Entry { Margin = margen };
-            Label lbl_monto = new Label { Text = "Monto:", Margin = margen };
+            Label lbl_monto = new Label { Text = "Monto:", Margin = margen, FontSize = 18, FontAttributes = FontAttributes.Bold };
             Entry ent_monto = new Entry { Margin = margen, Keyboard = Keyboard.Numeric };
+            Label lbl_picker = new Label { Text = "Categoria:", Margin = margen, FontSize = 18, FontAttributes = FontAttributes.Bold };
             Picker picker = new Picker { Title = "Categoria", Margin = margen };
             foreach (string colorName in NombreCategoria.Keys)
             {
                 picker.Items.Add(colorName);
             }
             // Picker por Defecto
-            picker.SelectedIndex = 0;
-
-            picker.SelectedIndexChanged += (sender, args) =>
-            {
-                if (picker.SelectedIndex == -1)
-                {
-                    //color por defecto
-                }
-                else
-                {
-                    // seleccionado picker.Items[picker.SelectedIndex];nameToColor[colorName];
-                }
-            };
+            picker.SelectedIndex = 9;
+            // Boton Limpiar
+            Button btn_limpiar = new Button { Text = "Limpiar", VerticalOptions = LayoutOptions.EndAndExpand, Margin = margen };
             var contentView = new ContentView
             {
                 Content = new StackLayout
@@ -78,13 +74,81 @@ namespace RegistroDeudas.Apps
                          ent_descripcion,
                          lbl_monto,
                          ent_monto,
-                         picker
+                         lbl_picker,
+                         picker,
+                         btn_limpiar
                     }
                 },
                 ControlTemplate = formato_template
             };
+            Appearing += (object sender, System.EventArgs e) => ent_descripcion.Focus();
+            ent_descripcion.Completed += (object sender, EventArgs e) => { ent_monto.Focus(); };
+            ent_monto.Focused += (sender, args) =>
+            {
+                ent_monto.Text = "";
+            };
+            ent_monto.Completed += (object sender, EventArgs e) => { ent_monto.Text = formatoPeso(ent_monto.Text.ToString()); };
             Content = contentView;
+            // Eventos Botones
+            btn_compra.Clicked += async (sender, e) =>
+            {
+                if (ent_descripcion.Text != "" && ent_monto.Text != "")
+                {
+                    var answer = await DisplayAlert("Advertencia", "¿ Esto es una Compra ?", "Si por supuesto", "NO");
+                    if (answer)
+                    {
+                        await DisplayAlert("Advertencia", "SI ES UNA COMPRA", "OK");
+                    }
+                }
+                else
+                {
+                    await DisplayAlert("Advertencia", "Porfavor Completar todos los datos", "OK");
+                }
+            };
+            btn_venta.Clicked += async (sender, e) =>
+            {
+                if (ent_descripcion.Text != "" && ent_monto.Text != "")
+                {
+                    var answer = await DisplayAlert("Advertencia", "¿ Esto es una Venta ?", "Si por supuesto", "NO");
+                    if (answer)
+                    {
+                        await DisplayAlert("Advertencia", "Esto es una Venta", "OK");
+                    }
+                }
+                else
+                {
+                    await DisplayAlert("Advertencia", "Porfavor Completar todos los datos", "OK");
+                }
+
+            };
+            btn_transferencia.Clicked += async (sender, e) =>
+            {
+                if (ent_descripcion.Text != "" && ent_monto.Text != "")
+                {
+                    var answer = await DisplayAlert("Advertencia", "¿ Esto es una Transferencia ?", "Si por supuesto", "NO");
+                    if (answer)
+                    {
+                        await DisplayAlert("Advertencia", "Esto es una Transferencia", "OK");
+                    }
+                }
+                else
+                {
+                    await DisplayAlert("Advertencia", "Porfavor Completar todos los datos", "OK");
+                }
+            };
+            btn_limpiar.Clicked += async (sender, e) =>
+            {
+                ent_descripcion.Text = "";
+                ent_monto.Text = "";
+            };
         }
+        private string formatoPeso(string valor)
+        {
+            int numero = Int32.Parse(valor);
+            return numero.ToString("C");
+        }
+
+
         class FormatoTemplate : Grid
         {
             public FormatoTemplate()
@@ -124,7 +188,7 @@ namespace RegistroDeudas.Apps
             }
         }
         ControlTemplate formato_template = new ControlTemplate(typeof(FormatoTemplate));
-        public static readonly BindableProperty HeaderTextProperty = BindableProperty.Create("HeaderText", typeof(string), typeof(Home), "Bienvenido a Registro Deudas");
+        public static readonly BindableProperty HeaderTextProperty = BindableProperty.Create("HeaderText", typeof(string), typeof(Home), "RegistroDeudas Historico");
 
         public string HeaderText
         {
